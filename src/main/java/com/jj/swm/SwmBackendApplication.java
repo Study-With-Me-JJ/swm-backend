c2f994022645317916cba4cb04ef745e665be124
choi <rosoa0475@gmail.com>
2024-11-28 20:54:53 +0900

    init: 프로젝트 시작

---
+++ src/main/java/com/jj/swm/SwmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jj.swm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwmBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwmBackendApplication.class, args);
+    }
+
+}
