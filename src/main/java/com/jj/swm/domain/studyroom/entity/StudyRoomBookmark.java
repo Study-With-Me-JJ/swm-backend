6c82807ff35e40e82a44d059ddc2f2411a58c110
HanYoonSoo <hdbstn3055@gmail.com>
2024-11-30 21:31:52 +0900

    feat: 스터디 룸 북마크 엔티티 생성

---
+++ src/main/java/com/jj/swm/domain/studyroom/entity/StudyRoomBookmark.java
@@ -0,0 +1,36 @@
+package com.jj.swm.domain.studyroom.entity;
+
+import com.jj.swm.domain.user.entity.User;
+import com.jj.swm.global.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@SQLDelete(sql = "UPDATE study_room_bookmark SET deleted_at = NOW() WHERE id = ?")
+@SQLRestriction("deleted_at is null")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Table(name = "study_room_bookmark")
+public class StudyRoomBookmark extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "study_room_id", nullable = false)
+    private StudyRoom studyRoom;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+}
