8c01ef076609b06465aae0facc670f02935f2911
HanYoonSoo <hdbstn3055@gmail.com>
2024-12-01 17:06:09 +0900

    Revert "Revert "[SWM-37] 스터디 룸 엔티티 예약시간, 최소/최대 인원 추가""

---
+++ src/main/java/com/jj/swm/domain/studyroom/entity/StudyRoomReserveType.java
@@ -0,0 +1,41 @@
+package com.jj.swm.domain.studyroom.entity;
+
+import com.jj.swm.domain.user.entity.User;
+import com.jj.swm.global.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@SQLDelete(sql = "UPDATE study_room_reserve_type SET deleted_at = NOW() WHERE id = ?")
+@SQLRestriction("deleted_at is null")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Table(name = "study_room_reserve_type")
+public class StudyRoomReserveType extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "max_headcount", nullable = false)
+    private int maxHeadcount;
+
+    @Column(name = "reservation_option", nullable = false)
+    private String reservationOption;
+
+    @Column(name = "price_per_hour", nullable = false)
+    private int pricePerHour;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "study_room_id", nullable = false)
+    private StudyRoom studyRoom;
+}
