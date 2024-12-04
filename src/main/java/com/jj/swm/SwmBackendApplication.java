29a1206e33835f715e4eba2389aa800c3992417e
ChoiJHyuk <rosoa0475@gmail.com>
2024-12-04 12:55:44 +0900

    fix: 컨트롤러 단위 테스트 시에 @EnableJpaAuditing 충돌 해결

--- src/main/java/com/jj/swm/SwmBackendApplication.java
+++ src/main/java/com/jj/swm/SwmBackendApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
 @EnableScheduling
-@EnableJpaAuditing
+@SpringBootApplication
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class SwmBackendApplication {
 
