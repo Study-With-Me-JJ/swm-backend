8ca424cb6572f43702fda61d7ba1a7a10d43633a
HanYoonSoo <hdbstn3055@gmail.com>
2024-12-10 22:12:04 +0900

    refactor: 스터디 룸 서비스 테스트 리팩토링

--- src/test/java/com/jj/swm/domain/studyroom/service/StudyRoomCommandServiceTest.java
+++ src/test/java/com/jj/swm/domain/studyroom/service/StudyRoomCommandServiceTest.java
@@ -2,10 +2,10 @@ package com.jj.swm.domain.studyroom.service;
 
 import com.jj.swm.IntegrationContainerSupporter;
 import com.jj.swm.domain.studyroom.StudyRoomFixture;
-import com.jj.swm.domain.studyroom.dto.response.StudyRoomLikeCreateResponse;
 import com.jj.swm.domain.studyroom.entity.StudyRoom;
 import com.jj.swm.domain.studyroom.repository.StudyRoomLikeRepository;
 import com.jj.swm.domain.studyroom.repository.StudyRoomRepository;
+import com.jj.swm.domain.studyroom.repository.StudyRoomReviewRepository;
 import com.jj.swm.domain.user.UserFixture;
 import com.jj.swm.domain.user.entity.User;
 import com.jj.swm.domain.user.repository.UserRepository;
@@ -28,20 +28,12 @@ public class StudyRoomCommandServiceTest extends IntegrationContainerSupporter {
 
     private static final int THREAD_COUNT = 100;
 
-    @Autowired
-    private StudyRoomCommandService studyRoomCommandService;
-
-    @Autowired
-    private StudyRoomLikeRepository likeRepository;
-
-    @Autowired
-    private StudyRoomRepository studyRoomRepository;
-
-    @Autowired
-    private UserRepository userRepository;
+    @Autowired private StudyRoomCommandService commandService;
+    @Autowired private StudyRoomLikeRepository likeRepository;
+    @Autowired private StudyRoomRepository studyRoomRepository;
+    @Autowired private UserRepository userRepository;
 
     private StudyRoom studyRoom;
-    private List<UUID> userUuids;
 
     private ExecutorService executorService;
     private CountDownLatch countDownLatch;
@@ -62,9 +54,9 @@ public class StudyRoomCommandServiceTest extends IntegrationContainerSupporter {
     @Test
     @DisplayName("스터디 룸 좋아요 동시성 테스트에 성공한다.")
     @Transactional(propagation = Propagation.NOT_SUPPORTED)
-    void studyRoom_like_concurrency_test() throws InterruptedException {
+    void studyRoom_like_concurrency_test_Success() throws InterruptedException {
         //given
-        userUuids = createTestUsers();
+        List<UUID> userUuids = createTestUsers(THREAD_COUNT);
         executorService = Executors.newFixedThreadPool(THREAD_COUNT);
         countDownLatch = new CountDownLatch(THREAD_COUNT);
 
@@ -73,7 +65,7 @@ public class StudyRoomCommandServiceTest extends IntegrationContainerSupporter {
             final UUID uuid = userUuids.get(i);
             executorService.submit(() -> {
                 try {
-                    studyRoomCommandService.createLike(studyRoom.getId(), uuid);
+                    commandService.createLike(studyRoom.getId(), uuid);
                 } catch (Exception e){
                     e.printStackTrace();
                 } finally {
@@ -86,7 +78,7 @@ public class StudyRoomCommandServiceTest extends IntegrationContainerSupporter {
         countDownLatch.await();
 
         // then
-        long likeCount = likeRepository.countStudyRoomLikeByStudyRoom(studyRoom);
+        int likeCount = likeRepository.countStudyRoomLikeByStudyRoom(studyRoom);
         assertThat(likeCount).isEqualTo(100); // 사용자마다 한 번씩 좋아요가 생성되어야 함
 
         studyRoom = studyRoomRepository.findById(studyRoom.getId()).get();
@@ -96,17 +88,14 @@ public class StudyRoomCommandServiceTest extends IntegrationContainerSupporter {
     @Test
     @DisplayName("스터디 룸 좋아요 취소 동시성 테스트에 성공한다.")
     @Transactional(propagation = Propagation.NOT_SUPPORTED)
-    void studyRoom_dislike_concurrency_test() throws InterruptedException {
+    void studyRoom_dislike_concurrency_test_Success() throws InterruptedException {
         //given
-        userUuids = createTestUsers();
+        List<UUID> userUuids = createTestUsers(THREAD_COUNT);
         executorService = Executors.newFixedThreadPool(THREAD_COUNT);
         countDownLatch = new CountDownLatch(THREAD_COUNT);
 
-        List<Long> studyRoomLikeIds = new ArrayList<>();
-
         for(UUID uuid : userUuids) {
-           StudyRoomLikeCreateResponse response = studyRoomCommandService.createLike(studyRoom.getId(), uuid);
-           studyRoomLikeIds.add(response.getStudyRoomLikeId());
+           commandService.createLike(studyRoom.getId(), uuid);
         }
 
         //when
@@ -114,7 +103,7 @@ public class StudyRoomCommandServiceTest extends IntegrationContainerSupporter {
             final UUID uuid = userUuids.get(i);
             executorService.submit(() -> {
                 try {
-                    studyRoomCommandService.unLike(studyRoom.getId(), uuid);
+                    commandService.unLike(studyRoom.getId(), uuid);
                 } catch (Exception e){
                     e.printStackTrace();
                 } finally {
@@ -127,16 +116,16 @@ public class StudyRoomCommandServiceTest extends IntegrationContainerSupporter {
         countDownLatch.await();
 
         // then
-        long likeCount = likeRepository.countStudyRoomLikeByStudyRoom(studyRoom);
+        int likeCount = likeRepository.countStudyRoomLikeByStudyRoom(studyRoom);
         assertThat(likeCount).isEqualTo(0); // 최종적으로 좋아요가 모두 취소되어야 함
 
         studyRoom = studyRoomRepository.findById(studyRoom.getId()).get();
         assertThat(studyRoom.getLikeCount()).isEqualTo(0);
     }
 
-    private List<UUID> createTestUsers() {
+    private List<UUID> createTestUsers(int size) {
         List<UUID> userUuids = new ArrayList<>();
-        for (int i = 0; i < THREAD_COUNT; i++) {
+        for (int i = 0; i < size; i++) {
             User user = UserFixture.createUserWithUUID();
             user = userRepository.save(user);
             userUuids.add(user.getId());
