34f0607cd1add1ef45213ff1150fcf7bda0f0a7d
HanYoonSoo <hdbstn3055@gmail.com>
2024-12-29 22:19:08 +0900

    feat: JWT 관련 예외 코드 추가

--- src/main/java/com/jj/swm/global/common/enums/ErrorCode.java
+++ src/main/java/com/jj/swm/global/common/enums/ErrorCode.java
@@ -10,8 +10,11 @@ public enum ErrorCode {
     NOT_FOUND(10000, HttpStatus.NOT_FOUND, "Not Found"),
     FORBIDDEN(10001, HttpStatus.FORBIDDEN, "Forbidden"),
     NOT_VALID(10002, HttpStatus.BAD_REQUEST, "Not Valid"),
-
-    INTERNAL_SERVER_ERROR(20000, HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error");
+    UNAUTHORIZED_USER(10003, HttpStatus.UNAUTHORIZED, "Unauthorized User"),
+    INVALID_TOKEN(10004, HttpStatus.UNAUTHORIZED, "Invalid Token"),
+    INVALID_JWT_SIGNATURE(10005, HttpStatus.UNAUTHORIZED, "Invalid JWT Signature"),
+    EXPIRED_TOKEN(10006, HttpStatus.UNAUTHORIZED, "Expired Token"),
+    INTERNAL_SERVER_ERROR(20000, HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error"),;
 
     private final Integer code;
     private final HttpStatus httpStatus;
