587c98f0d14671785db41daebdfe869f56dc2944
HanYoonSoo <hdbstn3055@gmail.com>
2024-12-29 22:19:30 +0900

    feat: 엑세스 토큰, 리프레시 토큰 ExpirationTime 추가

--- src/main/java/com/jj/swm/global/common/enums/ExpirationTime.java
+++ src/main/java/com/jj/swm/global/common/enums/ExpirationTime.java
@@ -7,8 +7,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ExpirationTime {
     EMAIL(600000), // 10분
-    EMAIL_VERIFIED(1800000); // 30분
+    EMAIL_VERIFIED(30 * 60 * 1000L), // 30분
+    ACCESS_TOKEN(30 * 60 * 1000L), // 30분
+    REFRESH_TOKEN(60 * 60 * 3 * 1000L); // 3시간
 
     private final long value;
-
 }
