8b494790b471330809346a731b897dead7e7f5dd
HanYoonSoo <hdbstn3055@gmail.com>
2024-12-29 22:19:54 +0900

    refactor: 코드 컨벤션에 맞게 수정

--- src/main/java/com/jj/swm/global/common/service/RedisService.java
+++ src/main/java/com/jj/swm/global/common/service/RedisService.java
@@ -13,7 +13,8 @@ public class RedisService {
     private final StringRedisTemplate stringRedisTemplate;
 
     public void setValueWithExpiration(String key, String value, long timeOut) {
-        stringRedisTemplate.opsForValue().set(key, value, Duration.ofMillis(timeOut));
+        stringRedisTemplate.opsForValue()
+                .set(key, value, Duration.ofMillis(timeOut));
     }
 
     public String getValue(String key) {
