919c8afafbee1d9583391d0ddc9923b7b6a29ff0
HanYoonSoo <hdbstn3055@gmail.com>
2024-12-30 21:58:20 +0900

    feat: User Fetch Join JPQL 추가

--- src/main/java/com/jj/swm/domain/user/repository/UserCredentialRepository.java
+++ src/main/java/com/jj/swm/domain/user/repository/UserCredentialRepository.java
@@ -2,8 +2,16 @@ package com.jj.swm.domain.user.repository;
 
 import com.jj.swm.domain.user.entity.UserCredential;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface UserCredentialRepository extends JpaRepository<UserCredential, Long> {
 
     boolean existsByLoginId(String loginId);
+
+    Optional<UserCredential> findByLoginId(String loginId);
+
+    @Query("select uc from UserCredential uc join fetch uc.user u where uc.value = ?1")
+    Optional<UserCredential> findByValue(String value);
 }
