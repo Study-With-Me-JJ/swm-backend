7344371a152d58d6f073040585539c84dd6c1eae
HanYoonSoo <hdbstn3055@gmail.com>
2025-01-10 23:33:46 +0900

    fix: 불필요한 패키지 참조 제거

--- src/test/java/com/jj/swm/domain/studyroom/fixture/StudyRoomLikeFixture.java
+++ src/test/java/com/jj/swm/domain/studyroom/fixture/StudyRoomLikeFixture.java
@@ -2,10 +2,7 @@ package com.jj.swm.domain.studyroom.fixture;
 
 import com.jj.swm.domain.studyroom.entity.StudyRoom;
 import com.jj.swm.domain.studyroom.entity.StudyRoomLike;
-import com.jj.swm.domain.user.entity.RoleType;
 import com.jj.swm.domain.user.entity.User;
-
-import java.util.UUID;
 
 public class StudyRoomLikeFixture {
 
