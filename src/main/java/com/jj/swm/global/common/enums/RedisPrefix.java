e849665f813cc25fb0787a2b752785b2f1973d0c
HanYoonSoo <hdbstn3055@gmail.com>
2025-01-11 17:06:31 +0900

    feat: 유저 패스워드 변경 로직 추가에 따른 이메일 인증 코드 추가

--- src/main/java/com/jj/swm/global/common/enums/RedisPrefix.java
+++ src/main/java/com/jj/swm/global/common/enums/RedisPrefix.java
@@ -6,7 +6,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum RedisPrefix {
-    AUTH_CODE("auth_code:");
+    EMAIL_AUTH_CODE("email_auth_code:"),
+    PASSWORD_AUTH_CODE("password_auth_code:"),
+    ID_AUTH_CODE("id_auth_code:");
 
     private final String value;
 }
--- src/main/java/com/jj/swm/global/common/service/EmailService.java
+++ src/main/java/com/jj/swm/global/common/service/EmailService.java
@@ -1,7 +1,6 @@
 package com.jj.swm.global.common.service;
 
-import com.jj.swm.global.common.enums.ErrorCode;
-import com.jj.swm.global.exception.GlobalException;
+import com.jj.swm.global.common.enums.EmailSendType;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
@@ -19,33 +18,51 @@ import java.util.concurrent.CompletableFuture;
 @RequiredArgsConstructor
 public class EmailService {
 
-    private static final String title = "스터디 윗 미 이메일 인증코드";
+    private static final String authCodeEmailTitle = "스터디 윗 미 인증코드";
+    private static final String retrieveEmailTitle = "스터디 윗 미 가입 이메일 안내";
 
     private final JavaMailSender mailSender;
 
     @Async("asyncExecutor")
-    public CompletableFuture<Boolean> sendEmail(String toEmail, String text) {
-        String htmlContent = createHTMLEmail(text);
+    public CompletableFuture<Boolean> sendAuthCodeEmail(String toEmail, String text, EmailSendType type) {
+        String htmlContent = createAuthCodeHTMLEmail(text, type);
 
         MimeMessage message = mailSender.createMimeMessage();
         try {
             MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
             helper.setTo(toEmail);
-            helper.setSubject(title);
+            helper.setSubject(authCodeEmailTitle);
             helper.setText(htmlContent, true);
 
             mailSender.send(message);
 
             return CompletableFuture.completedFuture(true);
         } catch (MessagingException | MailException e) {
-            log.error("MailService.sendEmail exception occur toEmail: {}, " +
-                    "title: {}, text: {}", toEmail, title, text);
+            log.error("MailService.sendAuthCodeEmail exception occur toEmail: {}, " +
+                    "title: {}, text: {}", toEmail, authCodeEmailTitle, text);
 
             return CompletableFuture.completedFuture(false);
         }
     }
 
-    private String createHTMLEmail(String text) {
+    @Async("asyncExecutor")
+    public void sendRetrieveEmail(String toEmail) {
+        String htmlContent = createRetrieveHTMLEmail(toEmail);
+
+        MimeMessage message = mailSender.createMimeMessage();
+        try{
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+            helper.setTo(toEmail);
+            helper.setSubject(retrieveEmailTitle);
+            helper.setText(htmlContent, true);
+
+            mailSender.send(message);
+        } catch (MessagingException | MailException e) {
+            log.error("MailService.sendAuthCodeEmail exception occur toEmail: {} ", toEmail);
+        }
+    }
+
+    private String createAuthCodeHTMLEmail(String text, EmailSendType type) {
         return String.format(
                 "<!DOCTYPE html>\n" +
                         "<html lang=\"ko\">\n" +
@@ -53,7 +70,7 @@ public class EmailService {
                         "    <meta charset=\"UTF-8\" />\n" +
                         "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\" />\n" +
                         "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />\n" +
-                        "    <title>이메일 인증코드 안내</title>\n" +
+                        "    <title>%s 인증코드 안내</title>\n" +
                         "  </head>\n" +
                         "  <body style=\"margin-top: 20px; padding: 0\">\n" +
                         "    <div\n" +
@@ -67,13 +84,13 @@ public class EmailService {
                         "      \"\n" +
                         "    >\n" +
                         "      <h2 style=\"font-size: 24px; color: #000; margin-bottom: 16px\">\n" +
-                        "        이메일 인증코드 안내\n" +
+                        "        %s 인증코드 안내\n" +
                         "      </h2>\n" +
                         "      <p style=\"font-size: 16px; color: #000; margin-bottom: 12px\">\n" +
                         "        안녕하세요, 스터디 윗 미입니다.\n" +
                         "      </p>\n" +
                         "      <p style=\"font-size: 16px; color: #000; margin-bottom: 20px\">\n" +
-                        "        고객님의 요청으로 이메일 인증코드 안내 메일을 발송했습니다.\n" +
+                        "        고객님의 요청으로 %s 인증코드 안내 메일을 발송했습니다.\n" +
                         "      </p>\n" +
                         "      <p style=\"font-size: 16px; color: #000; margin-bottom: 24px\">\n" +
                         "        아래 인증 코드를 통해 인증을 진행해 주세요.\n" +
@@ -102,7 +119,60 @@ public class EmailService {
                         "    </div>\n" +
                         "  </body>\n" +
                         "</html>",
-                text // 여기서 인증 코드 삽입
+                type.getType(), type.getType(), type.getType(), text // 여기서 인증 코드 삽입
+        );
+    }
+
+    private String createRetrieveHTMLEmail(String email) {
+        return String.format(
+                "<!DOCTYPE html>\n" +
+                        "<html lang=\"ko\">\n" +
+                        "  <head>\n" +
+                        "    <meta charset=\"UTF-8\" />\n" +
+                        "    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\" />\n" +
+                        "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />\n" +
+                        "    <title>가입 이메일 안내</title>\n" +
+                        "  </head>\n" +
+                        "  <body style=\"margin-top: 20px; padding: 0\">\n" +
+                        "    <div\n" +
+                        "      style=\"\n" +
+                        "        max-width: 600px;\n" +
+                        "        margin: 0 auto;\n" +
+                        "        padding: 20px;\n" +
+                        "        background-color: #ffffff;\n" +
+                        "        border-radius: 8px;\n" +
+                        "        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);\n" +
+                        "      \"\n" +
+                        "    >\n" +
+                        "      <h2 style=\"font-size: 24px; color: #000; margin-bottom: 16px\">\n" +
+                        "        가입된 이메일 안내\n" +
+                        "      </h2>\n" +
+                        "      <p style=\"font-size: 16px; color: #444; margin-bottom: 20px\">\n" +
+                        "        안녕하세요, 스터디 윗 미입니다.\n" +
+                        "      </p>\n" +
+                        "      <p style=\"font-size: 16px; color: #444; margin-bottom: 24px\">\n" +
+                        "        고객님은 아래 이메일 주소로 가입되어 계십니다.\n" +
+                        "      </p>\n" +
+                        "      <div\n" +
+                        "        style=\"\n" +
+                        "          padding: 16px;\n" +
+                        "          background-color: #f9f9f9;\n" +
+                        "          border-radius: 8px;\n" +
+                        "          text-align: center;\n" +
+                        "          margin-bottom: 24px;\n" +
+                        "        \"\n" +
+                        "      >\n" +
+                        "        <strong style=\"font-size: 18px; color: #018786;\">\n" +
+                        "          %s\n" +
+                        "        </strong>\n" +
+                        "      </div>\n" +
+                        "      <p style=\"font-size: 14px; color: #444; margin-bottom: 24px\">\n" +
+                        "        스터디 윗 미를 이용해 주셔서 감사합니다.<br />스터디 윗 미 팀 드림.\n" +
+                        "      </p>\n" +
+                        "    </div>\n" +
+                        "  </body>\n" +
+                        "</html>\n",
+                email
         );
     }
 }
