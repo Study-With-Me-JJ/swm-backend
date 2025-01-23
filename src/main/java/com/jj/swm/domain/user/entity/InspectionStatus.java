40a9d1561d429610887aef4afc373a48272d6a51
HanYoonSoo <hdbstn3055@gmail.com>
2025-01-23 21:52:33 +0900

    refactor: 검수 조건에 status 필드 추가

--- src/main/java/com/jj/swm/domain/user/entity/InspectionStatus.java
+++ src/main/java/com/jj/swm/domain/user/entity/InspectionStatus.java
@@ -1,5 +1,12 @@
 package com.jj.swm.domain.user.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum InspectionStatus {
-    PENDING, APPROVED, REJECTED
+    PENDING("PENDING"), APPROVED("APPROVED"), REJECTED("REJECTED");
+
+    private final String status;
 }
