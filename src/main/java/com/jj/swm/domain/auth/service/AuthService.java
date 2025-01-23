100d7792f4bce8c354271e692994ef714e610dc6
HanYoonSoo <hdbstn3055@gmail.com>
2025-01-23 21:45:39 +0900

    remove: 불필요한 패키지 제거

--- src/main/java/com/jj/swm/domain/auth/service/AuthService.java
+++ src/main/java/com/jj/swm/domain/auth/service/AuthService.java
@@ -10,7 +10,6 @@ import com.jj.swm.global.security.jwt.JwtProvider;
 import com.jj.swm.global.security.jwt.TokenRedisService;
 import jakarta.servlet.http.Cookie;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseCookie;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
