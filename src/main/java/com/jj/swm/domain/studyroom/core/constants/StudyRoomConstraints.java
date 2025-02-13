e6c21e7454db790256fbbe2ed16e8ed3d780d471
HanYoonSoo <hdbstn3055@gmail.com>
2025-02-13 16:19:11 +0900

    feat: 스터디 룸에서 활용되는 범용 상수 클래스 생성

---
+++ src/main/java/com/jj/swm/domain/studyroom/core/constants/StudyRoomConstraints.java
@@ -0,0 +1,10 @@
+package com.jj.swm.domain.studyroom.core.constants;
+
+public class StudyRoomConstraints {
+    public static final int TAG_LIMIT = 10;
+    public static final int IMAGE_LIMIT = 20;
+    public static final int TYPE_LIMIT = 3;
+    public static final int DAYOFF_LIMIT = 7;
+    public static final int MAX_HEADCOUNT = 50;
+    public static final int MAX_PRICE_PER_HOUR = 300000;
+}
