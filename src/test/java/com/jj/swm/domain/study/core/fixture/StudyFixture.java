923233a2ac85a299935268f0c5e2a0b94efa6309
ChoiJHyuk <rosoa0475@gmail.com>
2025-02-26 16:06:08 +0900

    feat: 기존 테스트 초기화

--- src/test/java/com/jj/swm/domain/study/core/fixture/StudyFixture.java
+++ src/test/java/com/jj/swm/domain/study/core/fixture/StudyFixture.java
@@ -1,24 +1,4 @@
 package com.jj.swm.domain.study.core.fixture;
 
-import com.jj.swm.domain.study.core.entity.Study;
-import com.jj.swm.domain.study.core.entity.StudyCategory;
-import com.jj.swm.domain.study.core.entity.StudyStatus;
-import com.jj.swm.domain.user.core.entity.User;
-
 public class StudyFixture {
-
-    public static final Long studyId = -1L;
-
-    public static Study createStudy(User user) {
-        return Study.builder()
-                .title("test_title")
-                .content("test_content")
-                .category(StudyCategory.ALGORITHM)
-                .likeCount(0)
-                .commentCount(0)
-                .status(StudyStatus.ACTIVE)
-                .viewCount(0)
-                .user(user)
-                .build();
-    }
 }
