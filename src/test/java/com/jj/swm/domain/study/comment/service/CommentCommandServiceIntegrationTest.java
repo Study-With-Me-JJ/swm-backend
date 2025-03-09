d5da7e9f7c79dacaef32a98a09b13b468137b385
ChoiJHyuk <rosoa0475@gmail.com>
2025-03-09 12:49:13 +0900

    test: 스터디 모집 댓글 수정 테스트

--- src/test/java/com/jj/swm/domain/study/comment/service/CommentCommandServiceIntegrationTest.java
+++ src/test/java/com/jj/swm/domain/study/comment/service/CommentCommandServiceIntegrationTest.java
@@ -3,6 +3,7 @@ package com.jj.swm.domain.study.comment.service;
 import com.jj.swm.IntegrationContainerSupporter;
 import com.jj.swm.domain.study.comment.dto.request.UpsertCommentRequest;
 import com.jj.swm.domain.study.comment.dto.response.CreateCommentResponse;
+import com.jj.swm.domain.study.comment.dto.response.UpdateCommentResponse;
 import com.jj.swm.domain.study.comment.entity.StudyComment;
 import com.jj.swm.domain.study.comment.fixture.entity.CommentFixture;
 import com.jj.swm.domain.study.comment.fixture.request.CommentRequestFixture;
@@ -18,6 +19,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CommentCommandServiceIntegrationTest extends IntegrationContainerSupporter {
@@ -39,20 +41,20 @@ public class CommentCommandServiceIntegrationTest extends IntegrationContainerSu
     // entity
     private User user;
     private Study study;
-    private StudyComment parent;
+    private StudyComment comment;
 
     @BeforeEach
     void setUp() {
         user = userRepository.save(UserFixture.createUser());
         study = studyRepository.save(StudyFixture.buildStudy(user));
-        parent = commentRepository.save(CommentFixture.buildStudyComment(user, study));
+        comment = commentRepository.save(CommentFixture.buildStudyComment(user, study));
     }
 
     @Test
-    @DisplayName("스터디 모집 댓글 생성에 성공한다")
+    @DisplayName("스터디 모집 댓글 생성에 성공한다.")
     void addComment_Success() {
         //given
-        UpsertCommentRequest createRequest = CommentRequestFixture.buildUpsertCommentRequest();
+        UpsertCommentRequest createRequest = CommentRequestFixture.buildCreateCommentRequest();
 
         //when
         CreateCommentResponse response = commentCommandService.addComment(
@@ -67,16 +69,16 @@ public class CommentCommandServiceIntegrationTest extends IntegrationContainerSu
     }
 
     @Test
-    @DisplayName("스터디 모집 대댓글 생성에 성공한다")
+    @DisplayName("스터디 모집 대댓글 생성에 성공한다.")
     void addComment_WithParentId_Success() {
         //given
-        UpsertCommentRequest createRequest = CommentRequestFixture.buildUpsertCommentRequest();
+        UpsertCommentRequest createRequest = CommentRequestFixture.buildCreateCommentRequest();
 
         //when
         CreateCommentResponse response = commentCommandService.addComment(
                 user.getId(),
                 study.getId(),
-                parent.getId(),
+                comment.getId(),
                 createRequest
         );
 
@@ -84,18 +86,18 @@ public class CommentCommandServiceIntegrationTest extends IntegrationContainerSu
         assertEquals(2L, response.getCommentId());
 
         StudyComment reply = commentRepository.findById(2L).get();
-        assertEquals(parent.getId(), reply.getParent().getId());
+        assertEquals(comment.getId(), reply.getParent().getId());
     }
 
     @Test
-    @DisplayName("스터디 모집 대댓글 id에 대해 대댓글을 생성해도 성공한다")
+    @DisplayName("스터디 모집 대댓글 id에 대해 대댓글을 생성해도 성공한다.")
     void addComment_WithReplyIdForParent_Success() {
         //given
-        UpsertCommentRequest createRequest = CommentRequestFixture.buildUpsertCommentRequest();
+        UpsertCommentRequest createRequest = CommentRequestFixture.buildCreateCommentRequest();
         Long replyId = commentCommandService.addComment(
                 user.getId(),
                 study.getId(),
-                parent.getId(),
+                comment.getId(),
                 createRequest
         ).getCommentId();
 
@@ -111,7 +113,23 @@ public class CommentCommandServiceIntegrationTest extends IntegrationContainerSu
         assertEquals(3L, response.getCommentId());
 
         StudyComment reply = commentRepository.findById(3L).get();
-        assertEquals(parent.getId(), reply.getParent().getId());
+        assertEquals(comment.getId(), reply.getParent().getId());
+    }
 
+    @Test
+    @DisplayName("스터디 모집 댓글 수정에 성공한다.")
+    void modifyComment_Success() {
+        //given
+        UpsertCommentRequest updateRequest = CommentRequestFixture.buildUpdateCommentRequest();
+
+        //when
+        UpdateCommentResponse response =
+                commentCommandService.modifyComment(user.getId(), comment.getId(), updateRequest);
+
+        //then
+        comment = commentRepository.findById(comment.getId()).get();
+
+        assertEquals(updateRequest.getContent(), comment.getContent());
+        assertNotEquals(comment.getCreatedAt(), response.getUpdatedAt());
     }
 }
