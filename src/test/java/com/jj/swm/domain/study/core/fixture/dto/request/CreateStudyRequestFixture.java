a4d059a46d1b07d2b2da4d1942f3b2abb946dd19
ChoiJHyuk <rosoa0475@gmail.com>
2025-03-17 12:42:36 +0900

    refactor: 코드 컨벤션에 따른 Fixture 클래스 리팩토링

---
+++ src/test/java/com/jj/swm/domain/study/core/fixture/dto/request/CreateStudyRequestFixture.java
@@ -0,0 +1,36 @@
+package com.jj.swm.domain.study.core.fixture.dto.request;
+
+import com.jj.swm.domain.study.core.dto.request.CreateStudyRequest;
+import com.jj.swm.domain.study.core.entity.StudyCategory;
+import com.jj.swm.domain.study.recruitmentposition.fixture.dto.request.CreateRecruitmentPositionRequestFixture;
+
+import java.util.List;
+
+public class CreateStudyRequestFixture {
+
+    public static CreateStudyRequest create() {
+        return CreateStudyRequest.builder()
+                .title("test_title")
+                .content("test_content")
+                .openChatUrl("test_open_chat_url")
+                .category(StudyCategory.ALGORITHM)
+                .tags(List.of("test_tag1", "test_tag2"))
+                .imageUrls(List.of("http://test_image1.png", "http://test_image2.png"))
+                .createRecruitmentPositionRequests(List.of(
+                        CreateRecruitmentPositionRequestFixture.create(),
+                        CreateRecruitmentPositionRequestFixture.create()
+                )).build();
+    }
+
+    public static CreateStudyRequest createForNoTagImagesSuccess() {
+        return CreateStudyRequest.builder()
+                .title("test_title")
+                .content("test_content")
+                .openChatUrl("test_open_chat_url")
+                .category(StudyCategory.ALGORITHM)
+                .createRecruitmentPositionRequests(List.of(
+                        CreateRecruitmentPositionRequestFixture.create(),
+                        CreateRecruitmentPositionRequestFixture.create()
+                )).build();
+    }
+}
