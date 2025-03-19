62ce7d3e60170f88c59fbbff62c0e0f6a666dc09
ChoiJHyuk <rosoa0475@gmail.com>
2025-03-20 01:04:49 +0900

    fix: 사용 안하는 필드 제거

--- src/main/java/com/jj/swm/domain/study/recruitmentposition/constants/StudyParticipationConstants.java
+++ src/main/java/com/jj/swm/domain/study/recruitmentposition/constants/StudyParticipationConstants.java
@@ -2,5 +2,4 @@ package com.jj.swm.domain.study.recruitmentposition.constants;
 
 public class StudyParticipationConstants {
     public static final int LINK_LIMIT = 3;
-    public static final int FILE_LIMIT = 10;
 }
