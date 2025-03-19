468536cfd691b5aac6f6ba5caff1d97b5a3dc5be
ChoiJHyuk <rosoa0475@gmail.com>
2025-03-19 23:52:36 +0900

    fix: link 제한 개수 변경

--- src/main/java/com/jj/swm/domain/study/recruitmentposition/constants/StudyParticipationConstants.java
+++ src/main/java/com/jj/swm/domain/study/recruitmentposition/constants/StudyParticipationConstants.java
@@ -1,6 +1,6 @@
 package com.jj.swm.domain.study.recruitmentposition.constants;
 
 public class StudyParticipationConstants {
-    public static final int LINK_LIMIT = 10;
+    public static final int LINK_LIMIT = 3;
     public static final int FILE_LIMIT = 10;
 }
