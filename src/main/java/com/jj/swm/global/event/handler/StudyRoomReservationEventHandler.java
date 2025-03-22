c3b68cb763f4d5dc9b656857a2efcf15723e0393
HanYoonSoo <hdbstn3055@gmail.com>
2025-03-22 16:26:01 +0900

    feat: 스터디 룸 예약 이벤트 핸들러 생성

---
+++ src/main/java/com/jj/swm/global/event/handler/StudyRoomReservationEventHandler.java
@@ -0,0 +1,31 @@
+package com.jj.swm.global.event.handler;
+
+import com.jj.swm.domain.studyroom.reservation.dto.event.StudyRoomReservationRequestEvent;
+import com.jj.swm.global.common.service.KakaoNotificationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class StudyRoomReservationEventHandler {
+
+    private final KakaoNotificationService kakaoNotificationService;
+
+    @TransactionalEventListener(classes = StudyRoomReservationRequestEvent.class, phase = TransactionPhase.AFTER_COMMIT)
+    public void studyRoomReservationRequestEventAfterCommitHandler(StudyRoomReservationRequestEvent event) {
+        kakaoNotificationService.sendStudyRoomReservationRequestNotification(event).thenAccept(success -> {
+            if(!success){
+                log.error("카카오 알림 전송 오류, time: {}", LocalDateTime.now(ZoneId.of("Asia/Seoul")));
+            } else {
+                log.info("카카오 알림 전송 성공, time: {}", LocalDateTime.now(ZoneId.of("Asia/Seoul")));
+            }
+        });
+    }
+}
