b3ca42cf1df5fe432da004af0e8c41140e39d85d
HanYoonSoo <hdbstn3055@gmail.com>
2025-03-24 21:53:23 +0900

    feat: 스터디 룸 예약 정보 id 및 유저 id 기반 조회 메서드 생성

--- src/main/java/com/jj/swm/domain/studyroom/reservation/repository/StudyRoomReservationInfoRepository.java
+++ src/main/java/com/jj/swm/domain/studyroom/reservation/repository/StudyRoomReservationInfoRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public interface StudyRoomReservationInfoRepository extends JpaRepository<StudyRoomReservationInfo, Long> {
 
     @Query("select s from StudyRoomReservationInfo s left join fetch s.studyRoom where s.id = ?1")
     Optional<StudyRoomReservationInfo> findByIdWithStudyRoom(Long studyRoomReservationInfoId);
+
+    Optional<StudyRoomReservationInfo> findByIdAndUserId(Long studyRoomReservationInfoId, UUID userId);
 }
