6406c6b92747568f9c173c3d725566d2084b0c5b
HanYoonSoo <hdbstn3055@gmail.com>
2025-03-24 21:15:16 +0900

    feat: 스터디 룸 예약 신청 응답 이벤트 핸들러 코드 작성

--- src/main/java/com/jj/swm/global/event/handler/StudyRoomReservationEventHandler.java
+++ src/main/java/com/jj/swm/global/event/handler/StudyRoomReservationEventHandler.java
@@ -1,6 +1,7 @@
 package com.jj.swm.global.event.handler;
 
 import com.jj.swm.domain.studyroom.reservation.dto.event.StudyRoomReservationRequestEvent;
+import com.jj.swm.domain.studyroom.reservation.dto.event.StudyRoomReservationResponseEvent;
 import com.jj.swm.global.common.service.KakaoNotificationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,6 +22,17 @@ public class StudyRoomReservationEventHandler {
     @TransactionalEventListener(classes = StudyRoomReservationRequestEvent.class, phase = TransactionPhase.AFTER_COMMIT)
     public void studyRoomReservationRequestEventAfterCommitHandler(StudyRoomReservationRequestEvent event) {
         kakaoNotificationService.sendStudyRoomReservationRequestNotification(event).thenAccept(success -> {
+            if(!success){
+                log.error("카카오 알림 전송 오류, time: {}", LocalDateTime.now(ZoneId.of("Asia/Seoul")));
+            } else {
+                log.info("카카오 알림 전송 성공, time: {}", LocalDateTime.now(ZoneId.of("Asia/Seoul")));
+            }
+        });
+    }
+
+    @TransactionalEventListener(classes = StudyRoomReservationResponseEvent.class, phase = TransactionPhase.AFTER_COMMIT)
+    public void studyRoomReservationResponseEventAfterCommitHandler(StudyRoomReservationResponseEvent event) {
+        kakaoNotificationService.sendStudyRoomReservationResponseNotification(event).thenAccept(success -> {
             if(!success){
                 log.error("카카오 알림 전송 오류, time: {}", LocalDateTime.now(ZoneId.of("Asia/Seoul")));
             } else {
