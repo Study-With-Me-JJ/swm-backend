feb03ffedb3d0378cfd5ddfdc0820476b3614bf1
HanYoonSoo <hdbstn3055@gmail.com>
2025-03-24 21:16:51 +0900

    feat: 스터디 룸 예약 신청 승인/거절 API 인증 제외 작업

--- src/main/java/com/jj/swm/global/config/SecurityConfig.java
+++ src/main/java/com/jj/swm/global/config/SecurityConfig.java
@@ -60,6 +60,7 @@ public class SecurityConfig {
                         .requestMatchers(AllowedPaths.getAllowedPaths()).permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/studyroom/user/**").authenticated()
                         .requestMatchers(HttpMethod.GET, "/api/v1/studyroom/**").permitAll()
+                        .requestMatchers(HttpMethod.PATCH, "/api/v1/studyroom/reservation/owner/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/study/user/**").authenticated()
                         .requestMatchers(HttpMethod.GET, "/api/v1/study/**", "/api/v1/comment/**").permitAll()
                         .requestMatchers("/api/v1/files/**").authenticated()
