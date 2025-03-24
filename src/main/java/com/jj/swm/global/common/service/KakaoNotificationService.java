01a21a61ec76f3b1965654b69c82c0b4cfb03c99
HanYoonSoo <hdbstn3055@gmail.com>
2025-03-24 21:11:25 +0900

    feat: 스터디 룸 예약 신청, 응답 알림톡 메서드 생성

--- src/main/java/com/jj/swm/global/common/service/KakaoNotificationService.java
+++ src/main/java/com/jj/swm/global/common/service/KakaoNotificationService.java
@@ -1,6 +1,7 @@
 package com.jj.swm.global.common.service;
 
 import com.jj.swm.domain.studyroom.reservation.dto.event.StudyRoomReservationRequestEvent;
+import com.jj.swm.domain.studyroom.reservation.dto.event.StudyRoomReservationResponseEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,11 @@ import java.util.concurrent.CompletableFuture;
 @RequiredArgsConstructor
 public class KakaoNotificationService {
     public CompletableFuture<Boolean> sendStudyRoomReservationRequestNotification(StudyRoomReservationRequestEvent event) {
+        log.info("카카오 알림 전송 성공, time: {}", LocalDateTime.now(ZoneId.of("Asia/Seoul")));
+        return CompletableFuture.completedFuture(true);
+    }
+
+    public CompletableFuture<Boolean> sendStudyRoomReservationResponseNotification(StudyRoomReservationResponseEvent event) {
         log.info("카카오 알림 전송 성공, time: {}", LocalDateTime.now(ZoneId.of("Asia/Seoul")));
         return CompletableFuture.completedFuture(true);
     }
