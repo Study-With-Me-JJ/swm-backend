b83e1ee365a83a89e3ef98f8efa19cf4a6ef7430
HanYoonSoo <hdbstn3055@gmail.com>
2025-03-24 21:17:21 +0900

    feat: 스터디 룸 예약 정보 조회 Repository 메소드 작성

--- src/main/java/com/jj/swm/domain/studyroom/reservation/repository/StudyRoomReservationInfoRepository.java
+++ src/main/java/com/jj/swm/domain/studyroom/reservation/repository/StudyRoomReservationInfoRepository.java
@@ -2,6 +2,12 @@ package com.jj.swm.domain.studyroom.reservation.repository;
 
 import com.jj.swm.domain.studyroom.reservation.entity.StudyRoomReservationInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface StudyRoomReservationInfoRepository extends JpaRepository<StudyRoomReservationInfo, Long> {
+
+    @Query("select s from StudyRoomReservationInfo s left join fetch s.studyRoom where s.id = ?1")
+    Optional<StudyRoomReservationInfo> findByIdWithStudyRoom(Long studyRoomReservationInfoId);
 }
