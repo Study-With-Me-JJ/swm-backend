57c0177b820912f5fb6d523bf25c55559e57edcc
HanYoonSoo <hdbstn3055@gmail.com>
2025-03-25 13:22:12 +0900

    feat: 스터디 룸 예약 승인 상태에 취소 추가

--- src/main/java/com/jj/swm/domain/studyroom/reservation/entity/ApprovalStatus.java
+++ src/main/java/com/jj/swm/domain/studyroom/reservation/entity/ApprovalStatus.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ApprovalStatus {
-    WAITING("WAITING"), APPROVED("APPROVED"), REJECTED("REJECTED");
+    WAITING("WAITING"), APPROVED("APPROVED"), REJECTED("REJECTED"), CANCELED("CANCELED");
 
     private final String status;
 }
