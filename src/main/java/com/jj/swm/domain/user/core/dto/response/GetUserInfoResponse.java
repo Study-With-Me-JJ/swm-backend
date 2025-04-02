bb32c0cc9e57c9087422dbdcab964a868c906a72
HanYoonSoo <hdbstn3055@gmail.com>
2025-04-02 14:29:11 +0900

    refactor: 유저 정보 조회 시 유저 이메일도 반환

--- src/main/java/com/jj/swm/domain/user/core/dto/response/GetUserInfoResponse.java
+++ src/main/java/com/jj/swm/domain/user/core/dto/response/GetUserInfoResponse.java
@@ -1,6 +1,7 @@
 package com.jj.swm.domain.user.core.dto.response;
 
 import com.jj.swm.domain.user.core.entity.User;
+import com.jj.swm.domain.user.core.entity.UserCredential;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -13,13 +14,17 @@ public class GetUserInfoResponse {
     private UUID userId;
     private String nickname;
     private String name;
+    private String email;
     private String profileImageUrl;
 
-    public static GetUserInfoResponse from(User user) {
+    public static GetUserInfoResponse from(UserCredential userCredential) {
+        User user = userCredential.getUser();
+
         return GetUserInfoResponse.builder()
                 .userId(user.getId())
                 .nickname(user.getNickname())
                 .name(user.getName())
+                .email(userCredential.getLoginId())
                 .profileImageUrl(user.getProfileImageUrl())
                 .build();
     }
