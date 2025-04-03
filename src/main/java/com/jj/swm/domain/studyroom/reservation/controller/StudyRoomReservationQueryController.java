673f645fb810aaa2af0fe5073f2cf531e3ba0f89
HanYoonSoo <hdbstn3055@gmail.com>
2025-04-03 16:51:58 +0900

    feat: 스터디 룸 소유자 예약 목록 조회 컨트롤러 로직 작성

--- src/main/java/com/jj/swm/domain/studyroom/reservation/controller/StudyRoomReservationQueryController.java
+++ src/main/java/com/jj/swm/domain/studyroom/reservation/controller/StudyRoomReservationQueryController.java
@@ -1,14 +1,17 @@
 package com.jj.swm.domain.studyroom.reservation.controller;
 
 import com.jj.swm.domain.studyroom.reservation.dto.response.GetReservationInfoDetailsResponse;
+import com.jj.swm.domain.studyroom.reservation.dto.response.GetReservationInfoResponse;
 import com.jj.swm.domain.studyroom.reservation.service.StudyRoomReservationQueryService;
 import com.jj.swm.global.common.dto.ApiResponse;
+import com.jj.swm.global.common.dto.PageResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.UUID;
 
 @Tag(name = "StudyRoomReservationInfo", description = "<b>[스터디 룸 예약]</b> API")
 @RestController
@@ -29,9 +32,22 @@ public class StudyRoomReservationQueryController {
 
     @GetMapping("/v1/studyroom/reservation/{studyRoomReservationInfoId}")
     public ApiResponse<GetReservationInfoDetailsResponse> getReservationInfoDetails(
-            @PathVariable("studyRoomReservationInfoId") Long studyRoomReservationInfoId
+            @PathVariable("studyRoomReservationInfoId") Long studyRoomReservationInfoId, Principal principal
     ) {
-        GetReservationInfoDetailsResponse response = queryService.getReservationInfoDetails(studyRoomReservationInfoId);
+        GetReservationInfoDetailsResponse response
+                = queryService.getReservationInfoDetails(studyRoomReservationInfoId, UUID.fromString(principal.getName()));
+
+        return ApiResponse.ok(response);
+    }
+
+    @Secured("ROLE_ROOM_ADMIN")
+    @GetMapping("/v1/studyroom/reservation/owner")
+    public ApiResponse<PageResponse<GetReservationInfoResponse>> getReservationInfos(
+            Principal principal, @RequestParam(value = "pageNo", required = false, defaultValue = "0") int pageNo
+    ) {
+        PageResponse<GetReservationInfoResponse> response = queryService.getReservationInfos(
+                UUID.fromString(principal.getName()), pageNo
+        );
 
         return ApiResponse.ok(response);
     }
