a47a2fc052ce11f75e3d6c8d6c613d4e1420157a
HanYoonSoo <hdbstn3055@gmail.com>
2025-04-03 16:52:09 +0900

    feat: 스터디 룸 소유자 예약 목록 조회 비즈니스 로직 작성

--- src/main/java/com/jj/swm/domain/studyroom/reservation/service/StudyRoomReservationQueryService.java
+++ src/main/java/com/jj/swm/domain/studyroom/reservation/service/StudyRoomReservationQueryService.java
@@ -1,14 +1,27 @@
 package com.jj.swm.domain.studyroom.reservation.service;
 
+import com.jj.swm.domain.studyroom.core.entity.StudyRoom;
+import com.jj.swm.domain.studyroom.core.repository.StudyRoomRepository;
 import com.jj.swm.domain.studyroom.reservation.dto.response.GetReservationInfoDetailsResponse;
+import com.jj.swm.domain.studyroom.reservation.dto.response.GetReservationInfoResponse;
 import com.jj.swm.domain.studyroom.reservation.entity.StudyRoomReservationInfo;
 import com.jj.swm.domain.studyroom.reservation.repository.StudyRoomReservationInfoRepository;
+import com.jj.swm.domain.studyroom.reservation.repository.custom.ReservationInfoResponse;
+import com.jj.swm.global.common.constants.PageSize;
+import com.jj.swm.global.common.dto.PageResponse;
 import com.jj.swm.global.common.enums.ErrorCode;
 import com.jj.swm.global.exception.GlobalException;
 import com.jj.swm.global.security.jwt.TokenRedisService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +30,7 @@ public class StudyRoomReservationQueryService {
     private final TokenRedisService tokenRedisService;
 
     private final StudyRoomReservationInfoRepository reservationInfoRepository;
+    private final StudyRoomRepository studyRoomRepository;
 
     @Transactional(readOnly = true)
     public GetReservationInfoDetailsResponse getReservationInfoDetails(String reservationToken) {
@@ -30,10 +44,22 @@ public class StudyRoomReservationQueryService {
     }
 
     @Transactional(readOnly = true)
-    public GetReservationInfoDetailsResponse getReservationInfoDetails(Long studyRoomReservationInfoId) {
-        StudyRoomReservationInfo studyRoomReservationInfo = reservationInfoRepository.findByIdWithReserveType(studyRoomReservationInfoId)
+    public GetReservationInfoDetailsResponse getReservationInfoDetails(Long studyRoomReservationInfoId, UUID userId) {
+        StudyRoomReservationInfo studyRoomReservationInfo = reservationInfoRepository.findByIdAndUserIdWithReserveType(studyRoomReservationInfoId, userId)
                 .orElseThrow(() -> new GlobalException(ErrorCode.NOT_FOUND, "StudyRoomReservationInfo Not Found"));
 
         return GetReservationInfoDetailsResponse.from(studyRoomReservationInfo);
+    }
+
+    @Transactional(readOnly = true)
+    public PageResponse<GetReservationInfoResponse> getReservationInfos(UUID userId, int pageNo) {
+        List<Long> studyRoomIds = studyRoomRepository.findStudyRoomIdsByUserId(userId);
+
+        Pageable pageable = PageRequest.of(pageNo, PageSize.StudyRoomReservationInfo, Sort.by("id").descending());
+
+        Page<ReservationInfoResponse> pagedReservationInfos
+                = reservationInfoRepository.findPagedReservationInfoByStudyRoomIds(studyRoomIds, pageable);
+
+        return PageResponse.of(pagedReservationInfos, GetReservationInfoResponse::from);
     }
 }
