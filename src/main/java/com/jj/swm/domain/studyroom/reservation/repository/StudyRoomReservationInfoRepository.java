22cd41e5370cffebf91ddfc7f0a6d6ab594d3380
HanYoonSoo <hdbstn3055@gmail.com>
2025-04-03 16:49:40 +0900

    feat: 스터디 룸 소유자 예약 목록 조회 Repository 메소드 작성

--- src/main/java/com/jj/swm/domain/studyroom/reservation/repository/StudyRoomReservationInfoRepository.java
+++ src/main/java/com/jj/swm/domain/studyroom/reservation/repository/StudyRoomReservationInfoRepository.java
@@ -1,9 +1,13 @@
 package com.jj.swm.domain.studyroom.reservation.repository;
 
 import com.jj.swm.domain.studyroom.reservation.entity.StudyRoomReservationInfo;
+import com.jj.swm.domain.studyroom.reservation.repository.custom.ReservationInfoResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -16,4 +20,18 @@ public interface StudyRoomReservationInfoRepository extends JpaRepository<StudyR
 
     @Query("select s from StudyRoomReservationInfo s left join fetch s.studyRoomReserveType where s.id = ?1")
     Optional<StudyRoomReservationInfo> findByIdWithReserveType(Long studyRoomReservationInfoId);
+
+    @Query("select s from StudyRoomReservationInfo s left join fetch s.studyRoomReserveType where s.id = ?1 and s.user.id = ?2")
+    Optional<StudyRoomReservationInfo> findByIdAndUserIdWithReserveType(Long studyRoomReservationInfoId, UUID userId);
+
+    @Query(
+        """
+            select s.id as studyRoomReservationInfoId, s.reserverName as reserverName, s.reserverPhoneNumber as reserverPhoneNumber,
+                   s.checkInTime as checkInTime, s.studyRoom.title as title, s.approvalStatus as approvalStatus
+            from StudyRoomReservationInfo s
+            left join s.studyRoom
+            where s.studyRoom.id in ?1
+       """
+    )
+    Page<ReservationInfoResponse> findPagedReservationInfoByStudyRoomIds(List<Long> studyRoomIds, Pageable pageable);
 }
