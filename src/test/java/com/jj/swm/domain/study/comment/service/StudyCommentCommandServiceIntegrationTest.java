bfbfa4e42a9ba41357554e43b5d61b5679def503
ChoiJHyuk <rosoa0475@gmail.com>
2025-04-12 22:08:33 +0900

    refactor: 스터디 댓글 커맨드 서비스 통합 테스트 리팩토링

--- src/test/java/com/jj/swm/domain/study/comment/service/StudyCommentCommandServiceIntegrationTest.java
+++ src/test/java/com/jj/swm/domain/study/comment/service/StudyCommentCommandServiceIntegrationTest.java
@@ -2,7 +2,6 @@ package com.jj.swm.domain.study.comment.service;
 
 import com.jj.swm.IntegrationContainerSupporter;
 import com.jj.swm.domain.study.comment.dto.request.UpsertStudyCommentRequest;
-import com.jj.swm.domain.study.comment.dto.response.UpdateStudyCommentResponse;
 import com.jj.swm.domain.study.comment.entity.StudyComment;
 import com.jj.swm.domain.study.comment.fixture.dto.request.UpsertStudyCommentRequestFixture;
 import com.jj.swm.domain.study.comment.repository.StudyStudyCommentRepository;
@@ -44,15 +43,15 @@ public class StudyCommentCommandServiceIntegrationTest extends IntegrationContai
     private UserRepository userRepository;
 
     @Autowired
-    private StudyStudyCommentRepository commentRepository;
+    private StudyRepository studyRepository;
 
     @Autowired
-    private StudyRepository studyRepository;
+    private StudyStudyCommentRepository commentRepository;
 
     // entity
     private User user;
     private Long commentId;
-    private final Long studyId = 1L;
+    private final Long studyId = 1L; // setUp에서 생성한 study id
 
     private ExecutorService executorService;
     private CountDownLatch countDownLatch;
@@ -96,7 +95,7 @@ public class StudyCommentCommandServiceIntegrationTest extends IntegrationContai
 
     @Test
     @DisplayName("스터디 모집 대댓글 생성에 성공한다.")
-    void createComment_WithParentId_Success() {
+    void createComment_WhenReply_Success() {
         //when
         Long replyId = commentCommandService.createComment(
                 UpsertStudyCommentRequestFixture.create(),
@@ -115,7 +114,7 @@ public class StudyCommentCommandServiceIntegrationTest extends IntegrationContai
 
     @Test
     @DisplayName("스터디 모집 대댓글 id에 대해 대댓글을 생성해도 성공한다.")
-    void createComment_WithReplyIdForParent_Success() {
+    void createComment_WhenReplyWithReplyId_Success() {
         //given
         UpsertStudyCommentRequest createRequest = UpsertStudyCommentRequestFixture.create();
         Long replyId = commentCommandService.createComment(
@@ -167,10 +166,8 @@ public class StudyCommentCommandServiceIntegrationTest extends IntegrationContai
         executorService.shutdown();
 
         //then
-        assertEquals(THREAD_COUNT + 1, commentRepository.count()); // setUp에서 기본 생성에 의해 +1 설정
-
         Study study = studyRepository.findById(studyId).get();
-        assertEquals(THREAD_COUNT + 1, study.getCommentCount());
+        assertEquals(THREAD_COUNT + 1, study.getCommentCount()); // setUp에서 기본 생성에 의해 +1 설정
     }
 
     @Test
@@ -180,7 +177,7 @@ public class StudyCommentCommandServiceIntegrationTest extends IntegrationContai
         UpsertStudyCommentRequest updateRequest = UpsertStudyCommentRequestFixture.update();
 
         //when
-        UpdateStudyCommentResponse response = commentCommandService.updateComment(
+        commentCommandService.updateComment(
                 updateRequest,
                 commentId,
                 user.getId()
@@ -190,25 +187,13 @@ public class StudyCommentCommandServiceIntegrationTest extends IntegrationContai
         StudyComment comment = commentRepository.findById(commentId).get();
 
         assertEquals(updateRequest.getContent(), comment.getContent());
-        assertNotEquals(comment.getCreatedAt(), response.getUpdatedAt());
+        assertNotEquals(comment.getCreatedAt(), comment.getUpdatedAt());
     }
 
     @Test
     @DisplayName("스터디 모집 댓글 삭제에 성공한다.")
     void deleteComment_Success() {
-        //when
-        commentCommandService.deleteComment(commentId, user.getId());
-
-        //then
-        Study study = studyRepository.findById(studyId).get();
-        assertEquals(0, study.getCommentCount());
-
-        assertEquals(0, commentRepository.count());
-    }
-
-    @Test
-    @DisplayName("대댓글이 있어도 스터디 모집 댓글 삭제에 성공한다.")
-    void deleteComment_WithReply_Success() {
+        //given
         commentCommandService.createComment(
                 UpsertStudyCommentRequestFixture.create(),
                 studyId,
@@ -228,7 +213,7 @@ public class StudyCommentCommandServiceIntegrationTest extends IntegrationContai
 
     @Test
     @DisplayName("스터디 모집 대댓글 삭제에 성공한다.")
-    void deleteComment_WithReplyId_Success() {
+    void deleteComment_WhenReply_Success() {
         //given
         Long replyId = commentCommandService.createComment(
                 UpsertStudyCommentRequestFixture.create(),
