86ef3e64a0a2c8c558c4fa89dc2b8fe9ba9d0f8a
HanYoonSoo <hdbstn3055@gmail.com>
2025-04-29 21:30:12 +0900

    refactor: QueryDSL 사용에 의한 기존 메서드 삭제

--- src/main/java/com/jj/swm/domain/studyroom/reservation/repository/StudyRoomReservationInfoRepository.java
+++ src/main/java/com/jj/swm/domain/studyroom/reservation/repository/StudyRoomReservationInfoRepository.java
@@ -1,18 +1,14 @@
 package com.jj.swm.domain.studyroom.reservation.repository;
 
 import com.jj.swm.domain.studyroom.reservation.entity.StudyRoomReservationInfo;
-import com.jj.swm.domain.studyroom.reservation.repository.custom.OwnerReservationInfoResponse;
-import com.jj.swm.domain.studyroom.reservation.repository.custom.ReserverReservationInfoResponse;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import com.jj.swm.domain.studyroom.reservation.repository.custom.CustomReservationInfoRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface StudyRoomReservationInfoRepository extends JpaRepository<StudyRoomReservationInfo, Long> {
+public interface StudyRoomReservationInfoRepository extends JpaRepository<StudyRoomReservationInfo, Long>, CustomReservationInfoRepository {
 
     @Query("select s from StudyRoomReservationInfo s left join fetch s.studyRoom where s.id = ?1")
     Optional<StudyRoomReservationInfo> findByIdWithStudyRoom(Long studyRoomReservationInfoId);
@@ -24,26 +20,4 @@ public interface StudyRoomReservationInfoRepository extends JpaRepository<StudyR
 
     @Query("select s from StudyRoomReservationInfo s left join fetch s.studyRoomReserveType where s.id = ?1 and s.user.id = ?2")
     Optional<StudyRoomReservationInfo> findByIdAndUserIdWithReserveType(Long studyRoomReservationInfoId, UUID userId);
-
-    @Query(
-        """
-            select s.id as studyRoomReservationInfoId, s.reserverName as reserverName, s.reserverPhoneNumber as reserverPhoneNumber,
-                   s.checkInTime as checkInTime, s.studyRoom.title as title, s.approvalStatus as approvalStatus
-            from StudyRoomReservationInfo s
-            left join s.studyRoom
-            where s.studyRoom.id in ?1
-       """
-    )
-    Page<OwnerReservationInfoResponse> findPagedReservationInfoByStudyRoomIds(List<Long> studyRoomIds, Pageable pageable);
-
-    @Query(
-            """
-                select s.id as studyRoomReservationInfoId, s.reserverName as reserverName, s.studyRoom.phoneNumber as studyRoomPhoneNumber,
-                       s.checkInTime as checkInTime, s.studyRoom.title as title, s.approvalStatus as approvalStatus
-                from StudyRoomReservationInfo s
-                left join s.studyRoom
-                where s.user.id = ?1
-           """
-    )
-    Page<ReserverReservationInfoResponse> findPagedReservationInfoByUserId(UUID userId, Pageable pageable);
 }
