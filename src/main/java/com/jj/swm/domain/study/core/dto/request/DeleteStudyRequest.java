566906611a813aa7c55b6fc289a3acbef68f1c7b
ChoiJHyuk <rosoa0475@gmail.com>
2025-06-14 15:01:23 +0900

    docs: 스터디 코어 커맨드 컨트롤러 스웨거 변경

--- src/main/java/com/jj/swm/domain/study/core/dto/request/DeleteStudyRequest.java
+++ src/main/java/com/jj/swm/domain/study/core/dto/request/DeleteStudyRequest.java
@@ -1,6 +1,7 @@
 package com.jj.swm.domain.study.core.dto.request;
 
 import com.jj.swm.global.common.constants.PageSize;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.*;
@@ -15,5 +16,6 @@ public class DeleteStudyRequest {
 
     @NotEmpty
     @Size(max = PageSize.Study)
+    @Schema(description = "삭제할 스터디 ID 목록")
     private List<Long> studyIds;
 }
